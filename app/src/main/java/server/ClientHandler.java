ef93963b7dcf7a51f0f4354680fb418ea19d8f1d
Daniel Kishon <danielk1133557799@gmail.com>
2026-01-09 08:51:45 +0200

    completed Client handler
    Signed-off-by: Daniel Kishon <danielk1133557799@gmail.com>

--- app/src/main/java/server/ClientHandler.java
+++ app/src/main/java/server/ClientHandler.java
@@ -5,6 +5,7 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
+
 public class ClientHandler implements Runnable {
     private final Socket socket;
 
@@ -13,7 +14,7 @@ public class ClientHandler implements Runnable {
 
     private ClientType clientType = null;
     private volatile boolean isRunning = false;
-    private String sessionKey;
+    private ControlSession controlSession = null;
 
     private final Object hostPendingLock = new Object();
 
@@ -35,7 +36,7 @@ public class ClientHandler implements Runnable {
                     hostHandler();
                     break;
                 case VIEWER:
-
+                    viewerHandler();
                     break;
                 case NULL:
                     break;
@@ -46,6 +47,7 @@ public class ClientHandler implements Runnable {
             System.out.println(e.getMessage());
         }
         finally  {
+            if (this.clientType != ClientType.VIEWER)
             closeEverything();
         }
     }
@@ -53,51 +55,50 @@ public class ClientHandler implements Runnable {
     private ClientType identification() throws IOException {
         String identification = this.inputStream.readUTF().toUpperCase();
 
-        if (identification.equals(ClientType.HOST.name()) && identification.equals(ClientType.VIEWER.name())) {
+        if (!identification.equals(ClientType.HOST.name()) && !identification.equals(ClientType.VIEWER.name())) {
             sendMessage("identification has been failed :(");
             return ClientType.NULL;
         }
         sendMessage("enter desired session code");
-        return identification == ClientType.HOST.name() ? ClientType.HOST : ClientType.VIEWER;
+        return identification.equals(ClientType.HOST.name()) ? ClientType.HOST : ClientType.VIEWER;
     }
 
     
     private void hostHandler() throws IOException, InterruptedException {
-        this.sessionKey = recieveSessionKeyFromHost();
+        String sessionKey = recieveSessionKeyFromHost();
 
         RelayServer.addAWaitingHost(sessionKey, this);
         hostPendingConnection();
+        sendMessage("viewer has been found");
 
         ControlSession controlSession = RelayServer.getControlSessionByKey(sessionKey);
         ClientHandler viewerHandler = controlSession.getViewerHandler();
+
         // transfering data between the viewer to the host
         Thread viewerToHost = new Thread(() -> {
             try {
                 boolean keepRunning = true;
                 while (keepRunning) {
-                    String viewerInput = viewerHandler.inputStream.readUTF();
-                    keepRunning = viewerInput.equals("EXIT");
-                    this.outputStream.writeUTF(viewerInput);
+                    String viewerInput = viewerHandler.recieveMessage();
+                    keepRunning = !viewerInput.equals("EXIT");
+                    sendMessage(viewerInput);
                 }
-            } catch (IOException e) {
-                System.out.println(e.getMessage());
-                closeEverything();
-            }
-        });
+
+            } catch (IOException e) { System.out.println(e.getMessage()); }
+        }); 
         viewerToHost.start();
-        
+
         // transfering data between host to the viewer
         boolean keepRunning = true;
         while (keepRunning) {
-            String hostInput = this.inputStream.readUTF();
-            keepRunning = hostInput.equals("EXIT");
-            viewerHandler.outputStream.writeUTF(hostInput);;
+            String hostInput = recieveMessage();
+            keepRunning = !hostInput.equals("EXIT");
+            viewerHandler.sendMessage(hostInput);;
         }
     }
-
+    
     private void hostPendingConnection() throws InterruptedException {
         synchronized (this.hostPendingLock) {
-            this.isRunning = true;
             while (this.isRunning) {
                 this.hostPendingLock.wait();
             }
@@ -105,18 +106,49 @@ public class ClientHandler implements Runnable {
         }
     }
     
+    private void viewerHandler() throws IOException {
+        String sessionKey = recieveSessionKeyFromViewer();
+        ClientHandler hostHandler = RelayServer.removeAWaitingHost(sessionKey);
+
+        this.controlSession = new ControlSession(this, hostHandler);
+        RelayServer.addAnActiveSession(sessionKey, this.controlSession);
+
+        // notify host for an upcoming connection
+        synchronized (hostHandler.hostPendingLock) { 
+            hostHandler.isRunning = false;
+            hostHandler.hostPendingLock.notify();
+        }
+
+    }
+
+    
     private String recieveSessionKeyFromHost() throws IOException {
         while (true) {
-            String sessionCode = this.inputStream.readUTF();
-            if (!RelayServer.isKeyInActiveSessions(sessionCode) && !RelayServer.isKeyInWaitingHosts(sessionCode)) {
-                return sessionCode;
+            String sessionKey = this.inputStream.readUTF();
+            if (!RelayServer.isKeyInActiveSessions(sessionKey) && !RelayServer.isKeyInWaitingHosts(sessionKey)) {
+                return sessionKey;
             }
-            this.outputStream.writeUTF("Key is already in use, try using a different key");
+            sendMessage("Key is already in use, try using a different key");
+        }
+    }
+    
+    private String recieveSessionKeyFromViewer() throws IOException {
+        while (true) {
+            String sessionKey = this.inputStream.readUTF();
+
+            if (RelayServer.isKeyInWaitingHosts(sessionKey)) {
+                return sessionKey;
+            }
+            sendMessage("No waiting host recognized by this key was found");
         }
     }
 
     private void sendMessage(String msg) throws IOException {
         this.outputStream.writeUTF(msg);
+    }
+
+    private String recieveMessage() throws IOException {
+        return this.inputStream.readUTF();
     }
     
     private void closeEverything() {
--- app/src/main/java/server/RelayServer.java
+++ app/src/main/java/server/RelayServer.java
@@ -3,24 +3,34 @@ package server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
 import java.util.Map;
 
 public class RelayServer {
-    private final int LISTENING_PORT = 5000;
+    private static final int LISTENING_PORT = 5000;
     private static Map<String, ClientHandler> waitingHosts;
     private static Map<String, ControlSession> activeSessions;
     
 
     public static void main(String[] args) {
+        System.out.println("server has started");
+        startServer(LISTENING_PORT);
     }
 
     private static void startServer(int listeningPort) {
+        waitingHosts = new HashMap<String, ClientHandler>();
+        activeSessions = new HashMap<String, ControlSession>();
+        
         try (ServerSocket serverSocket = new ServerSocket(listeningPort)) {
             System.out.println("The server is litening on port: " + listeningPort);
 
             while (true) {
-                Socket socket = serverSocket.accept();
-                System.out.println("client has been connected from IP: " + socket.getInetAddress().getHostAddress());
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("client has been connected from IP: " + clientSocket.getInetAddress().getHostAddress());
+                ClientHandler clientHandler = new ClientHandler(clientSocket);
+
+                Thread thread = new Thread(clientHandler);
+                thread.start();
             }
         }
         catch (IOException e) {
@@ -28,19 +38,36 @@ public class RelayServer {
         }
     } 
 
-    public static boolean isKeyInActiveSessions(String keySession) {
-        return activeSessions.containsKey(keySession);
-    }
-
+    
     public static boolean isKeyInWaitingHosts(String keySession) {
         return waitingHosts.containsKey(keySession);
     }
-
+    
     public static void addAWaitingHost(String key, ClientHandler clientHandler) {
         if (!isKeyInWaitingHosts(key)) {
             waitingHosts.put(key, clientHandler);
         } else {
             throw new RuntimeException("waiting host already exists");
+        }
+    }
+
+    public static boolean isKeyInActiveSessions(String keySession) {
+        return activeSessions.containsKey(keySession);
+    }
+    
+    public static void addAnActiveSession(String key, ControlSession controlSession) {
+        if (!isKeyInActiveSessions(key)) {
+            activeSessions.put(key, controlSession);
+        } else {
+            throw new RuntimeException("active session key already exists");
+        }
+    }
+
+    public static ClientHandler removeAWaitingHost(String key) {
+        if (isKeyInWaitingHosts(key)) {
+            return waitingHosts.remove(key);
+        } else {
+            throw new RuntimeException("no such a waiting host");
         }
     }
 
